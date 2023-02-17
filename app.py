a7ac338556703645efdb5a856296a43208ad2f31
carlosortiz <carlos.ortiz@itxpertix.com>
2023-02-17 14:15:34 -0500

    including analysis features

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import galai as gal
 import torch
+import baseAnalysis as ba
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here.
@@ -15,15 +16,16 @@ def inference(model_inputs:dict) -> dict:
     global model
 
     # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
+    resume = model_inputs.get('resume', None)
     max_length = model_inputs.get('max_length', 60)
     top_p = model_inputs.get('top_p', None)
     new_doc = model_inputs.get('new_doc', False)
-    if prompt == None:
-        return {'message': "No prompt provided"}
+    if resume == None:
+        return {'message': "No resume provided"}
     
     # Run the model
-    output = model.generate(prompt,max_length=max_length,top_p=top_p,new_doc=new_doc)
+    #output = model.generate(prompt,max_length=max_length,top_p=top_p,new_doc=new_doc)
+    output = ba.extract_datamap(model,resume,max_length)
 
     result = {"output": output}
 
---
+++ baseAnalysis.py
@@ -0,0 +1,35 @@
+
+tech_extr = '\n\nQ: What software technologies are mentioned in the abstract above?\n\nA:'
+pgrm_extr = '\n\nQ: What programming languages are mentioned in the abstract above?\n\nA:'
+dtbs_extr = '\n\nQ: What databases are mentioned in the abstract above?\n\nA:'
+edu_extr  = '\n\nQ: What education institutes are mentioned in the abstract above?\n\nA:'
+
+startup_fit  = '\n\nQ: Is the resume above a good fit to startups?\n\nA:'
+profile_extr = '\n\nQ: Among Developer, Tester, Back End, Front End, BlockChain Developer, DataScience, Manager What is the best classification for the resume above?\n\nA:'
+str_of_work_yr = '\n\nQ: What year did start work the candidate of the abstract above?\n\nA:'
+
+EMPTY = ''
+
+query_map = {"technologies":tech_extr , "programming-languages":pgrm_extr,
+             "databases" : dtbs_extr , "education": edu_extr, "fit-to-startups": startup_fit,
+            "profile":profile_extr,"years-of-work":str_of_work_yr}
+
+def parse_answer(s):
+  try:
+     lidx = s.rindex('A:')
+  except ValueError:
+    return EMPTY
+
+  return s[lidx+2:]
+
+def extract_datamap(model,resume,max_length=400):
+    response = {} 
+
+    # extract all relevant information from the current resume
+    for key,value in query_map.items():
+        prompt = resume + value
+        generated_text = model.generate(prompt,max_length=max_length)
+        answer = parse_answer(generated_text)
+        response[key] = answer.strip()
+
+    return response
